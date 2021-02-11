57a49dd1b3fd778de43b76b4e7d1545a214db71c
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-02-11 12:32:58 +0100

    embeded prnt.sc image and using axios over node-fetch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fetch = require("node-fetch");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
 require("dotenv").config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
@@ -12,9 +14,11 @@ client.on('ready', () => {
 });
 
 client.on('message', async msg => {
+
     if (msg.content === 'ping') {
         msg.reply('Pong!');
     }
+
     if (msg.channel.id === "360783461860114434" || msg.channel.id === "361895356776054784") {
         if ((msg.content).toLowerCase() === "!prnt") {
             const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -24,23 +28,29 @@ client.on('message', async msg => {
                 array.push(ind);
             }
             let query = array.join("");
-            msg.channel.send(`https://prnt.sc/${query}`);
+            try {
+                let urlData = await axios.get(`https://prnt.sc/${query}`);
+                let image = cheerio.load(urlData.data)
+                let imageSource = image("img#screenshot-image")[0].attribs.src;
+                msg.channel.send(imageSource);
+            } catch(err) {
+                console.log(err)
+            }
         }
+
         if ((msg.content).toLowerCase().includes("alexa play")) {
             const message = msg.content.split(" ");
             if (message.length > 2) {
                 const replyMessage = (message.slice(2)).join(" ");
                 const youtubeURL = `https://decapi.me/youtube/videoid/${replyMessage}`;
                 try {
-                    let urlData = await fetch(youtubeURL);
-                    let data = await urlData.text();
+                    let urlData = await axios.get(youtubeURL);
+                    let data = urlData.data;
                     msg.channel.send(`https://www.youtube.com/watch?v=${data}`);
                 } catch (err) {
                     console.log(err)
                 }                
             }
         }
-    }
-    
+    }  
 });
-
