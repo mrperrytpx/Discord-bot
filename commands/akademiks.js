7f309ef21ac687c4a54d31cdbde85d51ec0ffc4f
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-02-19 23:52:04 +0100

    missing bracket

--- commands/akademiks.js
+++ commands/akademiks.js
@@ -6,7 +6,7 @@ const images = ["https://media.discordapp.net/attachments/780213705387802674/798
     "https://cdn.substack.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2Ffc874bd1-55b8-45d2-960b-b3f4ee96ada3_942x529.webp"]
 
 module.exports = function akademiks(msg) {
-    if (msg.content.split(" ").length === 50 && msg.content.includes("mater") {
+    if (msg.content.split(" ").length === 50 && msg.content.includes("mater")) {
         const index = Math.floor(Math.random() * images.length);
         msg.channel.send(images[index]);
     }
